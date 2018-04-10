ba81650d9a5cc7766ad44f8fb70e03c26b0e2b9f
luthermiller6042 <37706126+luthermiller6042@users.noreply.github.com>
2018-04-10 18:58:16 -0400

    remade job board you can look at it at https://luthermiller6042.github.io/jobboard/  it allows for the use of pictures feel free to use if you like it....

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import React from 'react';import ReactDOM from 'react-dom';import './index.css';import App from './App';import registerServiceWorker from './registerServiceWorker';import 'tachyons';ReactDOM.render (<App />,document.getElementById('root'));registerServiceWorker();
