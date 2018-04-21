ac65ea28748d609fdc4304ac23d612e7edbddbd0
Thimothé Lamoureux <aparadox@gmail.com>
2018-04-21 00:28:21 -0700

    Changes made to structure data in a better way and avoid html in robots.js Added myself in robots.js Bugs still exist in searching feature. Needs work to comply with deprecated fields and new fields

--- src/Card.js
+++ src/Card.js
@@ -1,26 +1,50 @@
 import React from 'react';
 
-const Card = ({name, website, jobTitle, location, id})=> {
-	
-return(
+const Card = ({key, user}) => {
 
-	<div className='tc bg-white grow w5  br3 pa3 ma4  bw2 shadow-5'>
-	 { id ?  id  : <img className="br-100 pa1 ba b--black h4 w4" alt='robots' src={`https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Avatar-128.png` }/> }
-		
-
-		<div>
-
-			<h2>{name}</h2>
-			<div className=" grow ">{website}</div>
-			<div>{jobTitle}</div>
-			<p>{location}</p>
+    let {id, img, name, jobTitle, location, links, deprecated_id, deprecated_website, deprecated_location} = user;
+    let cardClassNames = 'tc bg-white grow w5  br3 pa3 ma4  bw2 shadow-5';
+    let imgClassNames = 'br-100 pa1 ba b--black h4 w4';
+    let socialClassNames = 'h2 ma2';
 
 
-		</div>
+    // Using the new id var to filter the new developer submissions
+    if (id != undefined) {
+        return (
+            <div id='{id}' className={cardClassNames}>
+                <img className={imgClassNames} alt={`${name}'s profile`}
+                     src={img !== "" ? img : 'https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Avatar-128.png'}/>
+                <div>
+                    <h2>{name}</h2>
+                    <div>
+                        <a href={links.website} title="Website/Portfolio"><img className={socialClassNames}
+                                                                               src="assets/images/website.svg"/></a>
+                        <a href={links.github} title="Github page"><img className={socialClassNames}
+                                                                        src="assets/images/github.svg"/></a>
+                        <a href={links.linkedin} title="LinkedIn page"><img className={socialClassNames}
+                                                                            src="assets/images/linkedin.svg"/></a>
+                    </div>
+                    <div>{jobTitle}</div>
+                    <p>{location.city}, {location.state}, {location.country}</p>
+                </div>
+            </div>
+        );
+    }
 
-	</div>
+    return (
 
-);
+        <div className={cardClassNames}>
+            {deprecated_id ? deprecated_id : <img className='{imgClassNames}' alt='robots'
+                                                  src={`https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Avatar-128.png`}/>}
+            <div>
+                <h2>{name}</h2>
+                <div className="grow">{deprecated_website}</div>
+                <div>{jobTitle}</div>
+                <p>{deprecated_location}</p>
+            </div>
+        </div>
+
+    );
 }
 
 export default Card;
