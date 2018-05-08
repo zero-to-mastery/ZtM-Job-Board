df9fb6c9c5e279ef7f01f1a6e2c298d6a65cabe1
Ekidoxx <ekidoxx@gmail.com>
2018-05-09 01:53:12 +0200

    Overall update & redesign
    1. Renamed `robots.js` to `persons.js`
    2. Updated `persons.js` syntax to version 2.0
    3. Fixed bug of filtering by location, that was occurring after above point
    4. Redesigned to more modern look

--- src/CardList.js
+++ src/CardList.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Card from './Card';
 
 
-const CardList = ({robots}) => {
-    const cardCom = robots.map((user, i) => {
-        return (<Card key={i} user={user}/>);
-    });
+const CardList = ({persons}) => {
+  const cardCom = persons.map((user, i) => {
+    return (<Card key={i} user={user}/>);
+  });
 
-    return (
-        <div className="flex flex-wrap justify-around">
-            {cardCom}
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap justify-center">
+      {cardCom}
+    </div>
+  );
 }
+
 export default CardList;
--- src/SearchCategory.js
+++ src/SearchCategory.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import './SearchCategory.css'
-const SearchCategory = ({categoryChange}) =>{
-    return(
-        <div className="SearchCategory">
-            <div className="ratio-box">
-                Categories: 
-            </div>
-            <div className="ratio-box">
-                <input type="radio" name="category" value="location" onChange={categoryChange}/> Location  
-            </div>
-            <div className="ratio-box">
-                <input type="radio" name="category" value="name" onChange={categoryChange}/> Name
-            </div>
-            <div className="ratio-box">
-                <input type="radio" name="category" value="job title" onChange={categoryChange}/> Job Title
-            </div> 
-        </div>
-    )
+
+const SearchCategory = ({categoryChange}) => {
+  return(
+    <div className="dn db-l SearchCategory relative ma0 pa0 fr">
+      <div className="radio-box pa0 di">
+        <input type="radio" name="category" value="location" onChange={categoryChange}/> Location
+      </div>
+      <div className="radio-box pa0 di">
+        <input type="radio" name="category" value="name" defaultChecked onChange={categoryChange}/> Name
+      </div>
+      <div className="radio-box pa0 di">
+        <input type="radio" name="category" value="job title" onChange={categoryChange}/> Job Title
+      </div>
+    </div>
+  )
 }
 
 export default SearchCategory;
