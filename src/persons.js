b406bc3359afd4c1b1f23917a08f541f6587bebf
Denise Recofka <recofka@gmail.com>
2018-05-16 13:16:11 +0200

    fixing wrong brackets on code

--- src/persons.js
+++ src/persons.js
@@ -1222,7 +1222,7 @@ export const persons = [
   },
 
   {
-    id: [73],
+    id: 73,
     name: "Denise Recofka",
     img: "https://media.licdn.com/dms/image/C5103AQGK2vR45hNmRg/profile-displayphoto-shrink_200_200/0?e=1531958400&v=beta&t=TA6FXa3SVFcbhjcrJ47zwUiDIT6RlQYdO7w8Kuj7V2I",
     links: {
