78fd2b6b1e2d38087eecc8e3adf70e7b752bd923
Ekidoxx <ekidoxx@gmail.com>
2018-05-22 16:21:09 +0200

    Fixes

--- src/App.js
+++ src/App.js
@@ -58,13 +58,13 @@ class App extends Component {
             <span className="fw3">Job</span>
             <span className="fw7 custom--text-primary">Board</span>
           </h1>
-          <SearchCategory categoryChange={this.onCategoryChange}/>
-          <SearchBox searchChange={this.onSearchChange} searchCategory={this.state.category}/>
+          <SearchCategory categoryChange={this.onCategoryChange} />
+          <SearchBox searchChange={this.onSearchChange} searchCategory={this.state.category} />
         </header>
         <main className="flex-auto">
           <CardList persons={filteredPersons} />
         </main>
-        <footer className="w-100 h3 custom--bg-additional3 flex items-center justify-center justify-end-l z-2">
+        <footer className="custom--unselectable w-100 h3 flex items-center justify-center justify-end-l white custom--bg-additional3 z-2">
           <a href="https://github.com/zero-to-mastery/ZtM-Job-Board" title="Repository">
             <svg className="repo w2 h2" viewBox="0 0 12 16" version="1.1" aria-hidden="true">
               <path fillRule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path>
