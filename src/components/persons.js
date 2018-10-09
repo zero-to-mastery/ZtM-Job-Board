bc3684fef6d3286ded0c6af3c91381f7775c6984
Adriano <adriano.ianase@gmail.com>
2018-10-09 10:06:45 -0400

    adding me to the list ;)

--- src/components/persons.js
+++ src/components/persons.js
@@ -3450,4 +3450,19 @@ export const persons = [
       country: "Chile"
     }
   },
+  {
+    id: 208,
+    name: "Adriano Ianase",
+    img: "https://media.licdn.com/dms/image/C4E03AQGTfv08sv9fBA/profile-displayphoto-shrink_200_200/0?e=1543449600&v=beta&t=8V8fXTBPNah4oN2HVXp8Ybe6EeXBbIdBtxmkxxmt298",
+    links: {
+      linkedin: "https://www.linkedin.com/in/adriano-ianase/,
+      github: "https://github.com/adrianomi"
+    },
+    jobTitle: "Front-End Developer",
+    location: {
+      city: "Campo Grande",
+      state: "Mato Grosso do Sul",
+      country: "Brazil"
+    }
+  },
 ];
