c187073cff387f971d233440a7587cf4ba41845c
lparra <l.parra@live.com>
2018-10-14 19:26:35 -0400

    Add my name to persons.js

--- src/components/persons.js
+++ src/components/persons.js
@@ -2747,4 +2747,17 @@ export const persons = [
     jobTitle: "Software Engineer",
     location: { city: "Toronto", state: "Ontario", country: "Canada" }
   }
+    {
+    id: 213,
+    name: "Luis Parra",
+    img:
+      "https://avatars2.githubusercontent.com/u/16653744?s=400&u=44a21a2b09172c9f2d300ee51aa95c14d3c8c8b8&v=4",
+    links: {
+      website: "",
+      linkedin: "https://www.linkedin.com/in/parral/",
+      github: "https://github.com/lparra"
+    },
+    jobTitle: "Software Engineer",
+    location: { city: "Fort Lauderdale", state: "Florida", country: "United States of America" }
+  }
 ];
