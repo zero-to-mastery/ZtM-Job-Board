41993edadf0366fe226e469275ff1a9e7f0ca9f6
Taulant Shametaj <t.shametaj@gmail.com>
2019-02-02 22:50:18 +0100

    Adding a new person

--- src/components/persons.js
+++ src/components/persons.js
@@ -1,21 +1,5 @@
 export const persons = [
 
-{
-    id: 0,
-    name: "Taulant Shametaj :)",
-    img: "https://avatars2.githubusercontent.com/u/45207786?s=400&u=2444f25b1917724d33457950a7c23c8a6dad4f07&v=4",
-    links: {
-      website: "https://shametaj.github.io",
-      linkedin: "https://gr.linkedin.com/in/taulant-shametaj-6518a191",
-      github: "https://github.com/shametaj"
-    },
-    jobTitle: "Web Developer",
-    location: {
-      city: "Saarbrücken",
-      state: "",
-      country: "Germany"
-    }
-  },
 
   {
     id: 1,
@@ -3911,6 +3895,23 @@ export const persons = [
       city: "Lekki",
       state: "Lagos",
       country: "Nigeria"
+    }
+  },
+
+  {
+    id: 283,
+    name: "Taulant Shametaj :)",
+    img: "https://avatars2.githubusercontent.com/u/45207786?s=400&u=2444f25b1917724d33457950a7c23c8a6dad4f07&v=4",
+    links: {
+      website: "https://shametaj.github.io",
+      linkedin: "https://gr.linkedin.com/in/taulant-shametaj-6518a191",
+      github: "https://github.com/shametaj"
+    },
+    jobTitle: "Web Developer",
+    location: {
+      city: "Saarbrücken",
+      state: "",
+      country: "Germany"
     }
   },
 ];
