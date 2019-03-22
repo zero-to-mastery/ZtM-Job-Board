235c30c92b536bdad184fe8ad76f7d5490969efd
komv8i <phani.komaravolu@cchmc.org>
2019-03-22 11:49:59 -0400

    basic refactoring with prettier lint, fix package vulnerabilities

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Card from './Card';
 
-const CardList = ({persons}) => {
+const CardList = ({ persons }) => {
   const cardCom = persons.map((user, i) => {
-    return (<Card key={i} user={user}/>);
+    return <Card key={i} user={user} />;
   });
 
-  return (
-      <React.Fragment>
-        {cardCom}
-      </React.Fragment>
-    );
-}
+  return <React.Fragment>{cardCom}</React.Fragment>;
+};
 
 export default CardList;
