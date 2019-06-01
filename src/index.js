d2cc6099003567fe80206e23769aacf5c447efbc
https://github.com/afreekamode <afreekamode@gmail.com>
2019-06-02 00:43:27 +0100

    add sikiru profile to the right column

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render (<App />,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
