fa244b0f972cc0dbc78a81c92a1d04a9068a925f
F1r3byte <f1r3byte@yandex.com>
2019-07-25 23:16:38 +0200

    fixed disaster conflict

--- src/components/persons.js
+++ src/components/persons.js
@@ -1,21 +1,5 @@
 // IMPORTANT!
 // If you wish to change the name or location of the persons.json file, Please drop @matt#8280 (Discord) or
 // @mattcsmith (Github) a message, I rely on this file for another upcoming project, and would therefor
-// appreciate the heads up and opportunity to modify the code on the other project. 
-export const persons = require('../assets/persons.json')
-  {
-    id: PREVIOUS_PLUS_ONE,
-    name: "Yulin Wang",
-    img: "",
-    links: {
-      website: "https://yulin-profile.herokuapp.com/",
-      linkedin: "www.linkedin.com/in/javascript-developer-yulin",
-      github: "https://github.com/wangyulinaaron"
-    },
-    jobTitle: "Full Stack Developer",
-    location: {
-      city: "ChongQing",
-      state: "ChongQing",
-      country: "China"
-    }
-  },
+// appreciate the heads up and opportunity to modify the code on the other project.
+export const persons = require('../assets/persons.json');
