95af231dcff233aaa3090fa538113e1d03ed8e2f
Abhishek K <abhishekakade@gmail.com>
2020-03-01 04:50:51 +0530

    fix: fixed all map issues

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,65 +1,123 @@
-import React from "react";
-import { Map as LeafletMap, TileLayer, Marker } from 'react-leaflet';
+import React, { useEffect, useState } from "react";
+import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
+import { countriesWithNumOfDevsObj } from "./UsersDataCleanup";
 
-import { people } from "../assets/persons";
+// console.log(countriesWithNumOfDevsObj);
 
-let arr = [];
+// Array of country names and number of devs in those countries
+/* Needed to match country names from countriesWithNumOfDevsObj against 
+country names fetched from API to get their latitude and longitude for markers */
+const countryNamesAndNumOfDevsArr = Object.entries(countriesWithNumOfDevsObj);
 
-//This should be in the life-cycle method componentDIdMount but all efforts
-//to make it work from there wasn't successful, the state doesn't update properly
-//Now the api is fetched when the site runs. This is not good
+// console.log(countryNamesAndNumOfDevsArr);
 
-people.forEach(person => {
-  let location;
-  if (person.location.country) {
-    //remove unnecessary characters from person.location.country where possible
-    location = person.location.country.toLowerCase();
-    if (location.search(/\s(?=\(|<)/) !== -1)
-      location = location.slice(0, location.search(/\s(?=\(|<)/g));
-  }
+let centerLatLngArr = [];
 
-  // wont make a fetch request if location is not provided/undefined/empty
-  if (location) {
-    fetch(`https://restcountries.eu/rest/v2/name/${location}`)
+function SimpleMap({ zoom = 3 }) {
+  const [allCountriesLatLang, setAllCountriesLatLang] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://restcountries.eu/rest/v2/all`)
       .then(resp => resp.json())
-      .then(data => {
-        if (data.length === 1) {
-          arr.push({ lat: data[0].latlng[0], lng: data[0].latlng[1] });
-        } else {
-          let someCountry;
-          if (data.length) {
-            someCountry = data.filter(
-              country => country.name.toLowerCase() === location.toLowerCase()
-            );
-            if (someCountry.length === 1) {
-              // mostly students in india, becus of API returns
-              arr.push({
-                lat: someCountry[0].latlng[0],
-                lng: someCountry[0].latlng[1]
-              });
-            } else if (location === "united states") {
-              // hard coded for some students in usa, becus of API return
-              arr.push({ lat: data[1].latlng[0], lng: data[1].latlng[1] });
-            } else if (location === "uk") {
-              //hard coded for some students in uk,  becus of API returns
-              arr.push({ lat: data[3].latlng[0], lng: data[3].latlng[1] });
-            }
-          }
-        }
-      })
+      .then(data => setAllCountriesLatLang(data))
       .catch(err => console.error(err));
+  }, []);
+
+  // console.log(allCountriesLatLang);
+
+  let countriesLatLngArr = allCountriesLatLang.map(({ name, latlng }) => ({
+    name,
+    latlng
+  }));
+  // console.log(countriesLatLngArr);
+
+  /* 
+  Made separate variable for UK because name of UK in API is "United Kingdom 
+  of Great Britain and Northern Ireland" which probably none of the users will use
+  */
+  const UK = {
+    country: "United Kingdom",
+    latlng: [54, -2], // copied from API
+    numberOfDevs: countriesWithNumOfDevsObj["United Kingdom"]
+  };
+
+  // Two nested for loops are okay as array items will always be < 250 in both arrays
+  let finalArrayWithCountryAndLatLng = [];
+  function finalCountryAndLocationArray() {
+    for (let i = 0; i < countryNamesAndNumOfDevsArr.length; i++) {
+      for (let j = 0; j < countriesLatLngArr.length; j++) {
+        if (
+          countryNamesAndNumOfDevsArr[i][0].toLowerCase() ===
+          countriesLatLngArr[j].name.toLowerCase()
+        ) {
+          finalArrayWithCountryAndLatLng.push({
+            country: countryNamesAndNumOfDevsArr[i][0],
+            latlng: countriesLatLngArr[j].latlng,
+            numberOfDevs: countryNamesAndNumOfDevsArr[i][1]
+          });
+        }
+      }
+    }
+    // Add UK to final array of countries and co-ordinates
+    finalArrayWithCountryAndLatLng.push(UK);
+    return finalArrayWithCountryAndLatLng;
   }
-});
+  finalCountryAndLocationArray();
+  // console.log(finalArrayWithCountryAndLatLng);
 
-function SimpleMap({ zoom = 4 }) {
+  // DO NOT DELETE THIS COMMENTED OUT CODE BELOW
+  // Might need it to check for unexpected repeated entries which is easier with sorted countries
+  /*
+  let sorterFunction = finalArrayWithCountryAndLatLng.sort(function(a, b) {
+    var nameA = a.country.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.country.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    // names must be equal
+    return 0;
+  });
+  console.log(sorterFunction);
+  */
 
-  const center  =  (arr.length > 0) ? [arr[0].lat, arr[0].lng]: [ 55.378052, -3.435973 ] ;
+  const markersArray = finalArrayWithCountryAndLatLng.map(
+    ({ country, latlng, numberOfDevs }) => {
+      return (
+        <Marker
+          key={country}
+          position={[latlng[0], latlng[1]]}
+          color="royalblue"
+          title={
+            numberOfDevs === 1
+              ? numberOfDevs + " Developer from " + country
+              : numberOfDevs + " Developers from " + country
+          }
+          text={numberOfDevs}
+        >
+          <Popup>
+            {numberOfDevs === 1
+              ? numberOfDevs + " Developer from " + country
+              : numberOfDevs + " Developers from " + country}
+          </Popup>
+        </Marker>
+      );
+    }
+  );
+
+  const center =
+    centerLatLngArr.length > 0
+      ? [centerLatLngArr[0].lat, centerLatLngArr[0].lng]
+      : [55.378052, -3.435973];
 
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
+    <div style={{ height: "90vh", width: "100%", margin: 0 }}>
       <LeafletMap
         center={center}
         zoom={zoom}
+        minZoom={1}
         maxZoom={10}
         attributionControl={true}
         zoomControl={true}
@@ -70,18 +128,10 @@ function SimpleMap({ zoom = 4 }) {
         easeLinearity={0.35}
       >
         <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         />
-        { 
-            arr.map((location, i) => (
-              <Marker 
-                key={i}
-                position={[location.lat, location.lng]}>
-              </Marker>
-
-            ))
-        }
+        {markersArray}
       </LeafletMap>
     </div>
   );
