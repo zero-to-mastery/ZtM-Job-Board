1cd936eb3c414351f949fd26f34884106ff997a6
Abhishek K <abhishekakade@gmail.com>
2020-03-15 19:32:24 +0530

    chore: moved UsersDataCleanup to src/util

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,34 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
-import { countriesWithNumOfDevsObj } from "./UsersDataCleanup";
+import React, { useEffect, useState } from "react"
+import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet"
+import { countriesWithNumOfDevsObj } from "../util/UsersDataCleanup"
 
 // console.log(countriesWithNumOfDevsObj);
 
 // Array of country names and number of devs in those countries
 /* Needed to match country names from countriesWithNumOfDevsObj against 
 country names fetched from API to get their latitude and longitude for markers */
-const countryNamesAndNumOfDevsArr = Object.entries(countriesWithNumOfDevsObj);
+const countryNamesAndNumOfDevsArr = Object.entries(countriesWithNumOfDevsObj)
 
 // console.log(countryNamesAndNumOfDevsArr);
 
-let centerLatLngArr = [];
+let centerLatLngArr = []
 
 function SimpleMap({ zoom = 3 }) {
-  const [allCountriesLatLang, setAllCountriesLatLang] = useState([]);
+  const [allCountriesLatLang, setAllCountriesLatLang] = useState([])
 
   useEffect(() => {
     fetch(`https://restcountries.eu/rest/v2/all`)
       .then(resp => resp.json())
       .then(data => setAllCountriesLatLang(data))
-      .catch(err => console.error(err));
-  }, []);
+      .catch(err => console.error(err))
+  }, [])
 
   // console.log(allCountriesLatLang);
 
   let countriesLatLngArr = allCountriesLatLang.map(({ name, latlng }) => ({
     name,
     latlng
-  }));
+  }))
   // console.log(countriesLatLngArr);
 
   /* 
@@ -39,10 +39,10 @@ function SimpleMap({ zoom = 3 }) {
     country: "United Kingdom",
     latlng: [54, -2], // copied from API
     numberOfDevs: countriesWithNumOfDevsObj["United Kingdom"]
-  };
+  }
 
   // Two nested for loops are okay as array items will always be < 250 in both arrays
-  let finalArrayWithCountryAndLatLng = [];
+  let finalArrayWithCountryAndLatLng = []
   function finalCountryAndLocationArray() {
     for (let i = 0; i < countryNamesAndNumOfDevsArr.length; i++) {
       for (let j = 0; j < countriesLatLngArr.length; j++) {
@@ -54,15 +54,15 @@ function SimpleMap({ zoom = 3 }) {
             country: countryNamesAndNumOfDevsArr[i][0],
             latlng: countriesLatLngArr[j].latlng,
             numberOfDevs: countryNamesAndNumOfDevsArr[i][1]
-          });
+          })
         }
       }
     }
     // Add UK to final array of countries and co-ordinates
-    finalArrayWithCountryAndLatLng.push(UK);
-    return finalArrayWithCountryAndLatLng;
+    finalArrayWithCountryAndLatLng.push(UK)
+    return finalArrayWithCountryAndLatLng
   }
-  finalCountryAndLocationArray();
+  finalCountryAndLocationArray()
   // console.log(finalArrayWithCountryAndLatLng);
 
   // DO NOT DELETE THIS COMMENTED OUT CODE BELOW
@@ -103,14 +103,14 @@ function SimpleMap({ zoom = 3 }) {
               : numberOfDevs + " Developers from " + country}
           </Popup>
         </Marker>
-      );
+      )
     }
-  );
+  )
 
   const center =
     centerLatLngArr.length > 0
       ? [centerLatLngArr[0].lat, centerLatLngArr[0].lng]
-      : [55.378052, -3.435973];
+      : [55.378052, -3.435973]
 
   return (
     <div style={{ height: "90vh", width: "100%", margin: 0 }}>
@@ -134,7 +134,7 @@ function SimpleMap({ zoom = 3 }) {
         {markersArray}
       </LeafletMap>
     </div>
-  );
+  )
 }
 
-export default SimpleMap;
+export default SimpleMap
