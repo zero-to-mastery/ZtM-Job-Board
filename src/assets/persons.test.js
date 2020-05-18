c05af996abba67a43371b30c72ff5681bb0aeb59
Hung Nguyen <viethung1996@gmail.com>
2020-05-18 13:24:45 -0400

    feat: Add unit testing to persons.json
    This feature helps validate the id of the most recent added person, which is one of the most common failure in build process. Also manually fixed wrong id added previously & deleted irrelevant persons.json in src dir.

--- src/assets/persons.test.js
+++ src/assets/persons.test.js
@@ -1,7 +1,16 @@
 import * as PERSONS from "./persons.json"
 import jsonlint from "jsonlint"
 
-test("Persons object is a valid JSON object", () => {
-  const peopleString = JSON.stringify(PERSONS)
-  jsonlint.parse(peopleString)
+describe("Person object", () => {
+  it("is a valid JSON object", () => {
+    const peopleString = JSON.stringify(PERSONS)
+    jsonlint.parse(peopleString)
+  })
+
+  const peopleArray = PERSONS.people
+  it("has last id equal previous + 1", () => {
+    const lastPerson = peopleArray[peopleArray.length - 2]
+    const addedPerson = peopleArray[peopleArray.length - 1]
+    expect(addedPerson.id).toEqual(lastPerson.id + 1)
+  })
 })
