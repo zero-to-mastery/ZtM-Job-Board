bfddfba566635131fe8f96514c457cad3f061b32
Baisch91 <cbaisch91@outlook.com>
2020-07-01 11:43:09 -0400

    Adding my profile to the project

--- src/assets/persons.test.js
+++
@@ -1,7 +0,0 @@
-import { data } from "./persons.js"
-import jsonlint from "jsonlint"
-
-test("Persons object is a valid JSON object", () => {
-  const peopleString = JSON.stringify(data)
-  jsonlint.parse(peopleString)
-})
