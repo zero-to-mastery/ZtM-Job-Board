88a466f40fbd04f14cc52f399ff880e609f179aa
Avinashk <avinashk@logicmatter.com>
2020-08-24 21:24:06 +0530

    Added My Profile Avinash Konduri

--- src/assets/persons.js
+++ src/assets/persons.js
@@ -13358,5 +13358,21 @@ export const data = {
         "country": "South Africa"
       }
     },
+    {
+      "id": uuidv4(),
+      "name": "Avinash Konduri",
+      "img": "https://photos.app.goo.gl/u3zmdifVgoNhv7rQ9",
+      "links": {
+        "website": "https://avinashkonduri.github.io/PersonalSite/",
+        "linkedin": "https://www.linkedin.com/in/avinash-konduri-b212a711/",
+        "github": "https://github.com/avinashkonduri/"
+      },
+      "jobTitle": "Full Stack Developer",
+      "location": {
+        "city": "Hyderabad",
+        "state": "Telangana",
+        "country": "India"
+      }
+    },
   ]
 }
