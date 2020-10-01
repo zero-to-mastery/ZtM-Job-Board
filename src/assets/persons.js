3bc22c9f1a4529c4e5812f6de1382c2eb561fb39
Janiu <hyperjay55@gmail.com>
2020-10-01 11:32:13 +0100

    Added my profile

--- src/assets/persons.js
+++ src/assets/persons.js
@@ -17161,5 +17161,22 @@ export const data = {
       },
     },
 
+    {
+      id: uuidv4(),
+      name: "Jamiu Jimoh",
+      img: "https://avatars0.githubusercontent.com/u/60991680?v=4",
+      links: {
+        website: "",
+        linkedin: "https://www.linkedin.com/in/jamiu-jimoh/",
+        github: "https://github.com/JamiuJimoh",
+      },
+      jobTitle: "Full Stack/MERN Developer",
+      location: {
+        city: "Lagos",
+        state: "Lagos",
+        country: "Nigeria",
+      },
+    },
+
   ],
 }
