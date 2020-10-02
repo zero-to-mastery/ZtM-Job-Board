75db3cc53ccd22cb3273d04adc378cfbef80ff83
andreyscott <62455784+andreyscott@users.noreply.github.com>
2020-10-02 10:42:23 +0100

    fixing conflict

--- src/assets/persons.js
+++ src/assets/persons.js
@@ -16944,6 +16944,23 @@ export const data = {
         country: "Nigeria"
       },
     },
+      {
+    id: uuidv4(),
+    name: "Andrew osayande Irorere",
+    img: "https://avatars3.githubusercontent.com/u/62455784?v=4",
+    links: {
+      website: "https://andreyscott.github.io/index.index.html",
+      linkedin: "https://www.linkedin.com/in/andrey-scott-b63a601b8/",
+      github: "https://github.com/andreyscott",
+    },
+    jobTitle: "Full-stack Web developer",
+    location: {
+      city: "Benin City",
+      state: "Edo state",
+      country: "Nigeria"
+    }
+  }
+
   ]
 }
 
