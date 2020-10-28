de52921804f3bffb92b78d644ee5b3ce354ec138
AbhishekHuddar <abhishekhuddar@gmail.com>
2020-10-27 21:21:39 -0500

    Added my profile to persons.js for ZTM Job board

--- src/assets/persons.js
+++ src/assets/persons.js
@@ -27096,5 +27096,21 @@ export const data = {
         country: "Argentina"
       }
     },
+    {
+    id: uuidv4(),
+    name: "Abhishek Huddar",
+    img: "https://avatars1.githubusercontent.com/u/61642154?v=4",
+    links: {
+      website: "https://stoic-leavitt-12a377.netlify.app/",
+      linkedin: "https://www.linkedin.com/in/abhishek-huddar/",
+      github: "https://github.com/AbhishekHuddar"
+    },
+    jobTitle: "Full Stack Developer",
+    location: {
+      city: "Baton Rouge",
+      state: "Louisiana",
+      country: "USA"
+    }
+  }
   ],
 }
