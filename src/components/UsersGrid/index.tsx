104e493ec6e532394c948d06d3bd1b70224d9413
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2021-06-27 04:59:34 +0200

    refactoring usersfr

--- src/components/UsersGrid/index.tsx
+++ src/components/UsersGrid/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Card, useTheme } from '../../components/super-components/core';
+import { useDataPage } from '../../hooks/useDataPage';
 import { Theme } from '../super-components/core/interfaces/theme';
 import UserCard from './UserCard';
+import { Pagination } from './../super-components/core/components/Pagination/index';
 
 const useStyles = (theme: Theme) => {
   return {
@@ -17,15 +19,19 @@ const useStyles = (theme: Theme) => {
 };
 
 export const UsersGridContainer = (props: any) => {
-  const { people, style } = props;
+  const { data } = useDataPage();
+  const { style } = props;
   const { theme } = useTheme();
   const styles = useStyles(theme);
 
   return (
-    <Card style={{ ...styles.container, ...style }}>
-      {people.slice(0, 20).map((item: any, i: any) => (
-        <UserCard key={i} {...item} />
-      ))}
-    </Card>
+    <>
+      <Card style={{ ...styles.container, ...style }}>
+        {data.map((item: any, i: any) => (
+          <UserCard key={i} {...item} />
+        ))}
+      </Card>
+      <Pagination />
+    </>
   );
 };
