a649b04091e1700eaeb1d0fa60fa7d02750a1df9
Sophia Brandt <git@s-br.net>
2023-08-09 17:22:53 +0200

    build: switched to .prettierrcjson with default config from documentation
    It seems like using the `.prettierrc` file doesn't work reliably, as I've seen a pull request that changed files but didn't trigger
    the lint-staged script (with prettier). When I manually run the prettier script, it also does not consider the `semi:false`.
    
    Using `.prettierrc.json` seems to work, and it also has the advantage of being a `.json` file, so it can be formatted and displayed nicely.

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,20 +1,20 @@
-import React, { useRef, useEffect } from "react"
+import React, { useRef, useEffect } from 'react'
 
 function Canvas() {
-  const canvasRef = useRef(null)
+    const canvasRef = useRef(null)
 
-  useEffect(() => {
-    const canvas: any = canvasRef.current
-    const ctx = canvas.getContext("2d")
-    ctx.beginPath()
-    ctx.arc(10, 10, 5, 0, 2 * Math.PI)
-    ctx.strokeStyle = "rgb(500,0,0)"
-    ctx.stroke()
-    ctx.fillStyle = "rgb(500,0,0)"
-    ctx.fillRect(9, 15, 3, 10)
-  }, [])
+    useEffect(() => {
+        const canvas: any = canvasRef.current
+        const ctx = canvas.getContext('2d')
+        ctx.beginPath()
+        ctx.arc(10, 10, 5, 0, 2 * Math.PI)
+        ctx.strokeStyle = 'rgb(500,0,0)'
+        ctx.stroke()
+        ctx.fillStyle = 'rgb(500,0,0)'
+        ctx.fillRect(9, 15, 3, 10)
+    }, [])
 
-  return <canvas ref={canvasRef} />
+    return <canvas ref={canvasRef} />
 }
 
 export default Canvas
