47a8087c84d7b76d798ad0df541247f16820e7f6
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-08-09 17:42:19 +0200

    build: switched to .prettierrcjson with default config from documentation (#2939)
    It seems like using the `.prettierrc` file doesn't work reliably, as I've seen a pull request that changed files but didn't trigger
    the lint-staged script (with prettier). When I manually run the prettier script, it also does not consider the `semi:false`.
    
    Using `.prettierrc.json` seems to work, and it also has the advantage of being a `.json` file, so it can be formatted and displayed nicely.

--- src/submissions.test.js
+++ src/submissions.test.js
@@ -1,15 +1,18 @@
-import jsonlint from "jsonlint"
-import fs from "fs"
-import path from "path"
+import jsonlint from 'jsonlint'
+import fs from 'fs'
+import path from 'path'
 
-test("Submission files must be valid JSON", () => {
-  const JSON_SOURCE_DIR = "Submissions"
-  const submissionFiles = fs.readdirSync(path.resolve(JSON_SOURCE_DIR))
+test('Submission files must be valid JSON', () => {
+    const JSON_SOURCE_DIR = 'Submissions'
+    const submissionFiles = fs.readdirSync(path.resolve(JSON_SOURCE_DIR))
 
-  submissionFiles.forEach((file) => {
-    const fileContent = fs.readFileSync(path.resolve(JSON_SOURCE_DIR, file), {
-      encoding: "utf-8",
+    submissionFiles.forEach((file) => {
+        const fileContent = fs.readFileSync(
+            path.resolve(JSON_SOURCE_DIR, file),
+            {
+                encoding: 'utf-8',
+            }
+        )
+        jsonlint.parse(fileContent)
     })
-    jsonlint.parse(fileContent)
-  })
 })
--- src/util/pageNames.tsx
+++ src/util/pageNames.tsx
@@ -1,4 +1,4 @@
 export const pageNames = {
-  home: "Home",
-  map: "Map",
+    home: 'Home',
+    map: 'Map',
 }
