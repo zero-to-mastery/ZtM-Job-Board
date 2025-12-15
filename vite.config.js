5f962c30e6177cebb684e6a039010d0506d099ba
Sophia Brandt <git@s-br.net>
2025-12-15 20:01:43 +0100

    fix issues with migrating to vite
    - fix %PUBLIC_URL%
    - set default port to 3000 (same as CRA)
    - fix package-lock.json out of sync

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-  };
-});
+    return {
+        build: {
+            outDir: 'build',
+        },
+        server: {
+            port: 3000,
+        },
+        plugins: [react()],
+    }
+})
